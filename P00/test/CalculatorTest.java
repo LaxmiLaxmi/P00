bf88d8b8d748368e589b8c16d2df2eb0246df6a8
21009173 <21009173@DESKTOP-F2A8A8Q>
2022-05-31 13:30:54 +0800

    Updated with multiplication and division methods.

--- P00/test/CalculatorTest.java
+++ P00/test/CalculatorTest.java
@@ -26,7 +26,6 @@ public class CalculatorTest {
 
 	@Test
 	public void testAdd() {
-		//fail("Not yet implemented");
 		int a = 1234;
 		int b = 8765;
 		
@@ -36,5 +35,40 @@ public class CalculatorTest {
 		int expected = 9999;
 		assertEquals (expected,actual);
 	}
-
+	
+	
+	public void testSubtract() {
+		int a = 9876;
+		int b = 4321;
+		
+		Calculator cal = new Calculator();
+		int actual = cal.subtract(a, b);
+		
+		int expected = 5555;
+		assertEquals (expected, actual);
+	}
+	
+	
+	public void testMultiple() {
+		int a = 2;
+		int b = 5;
+		
+		Calculator cal = new Calculator();
+		int actual = cal.multiple(a, b);
+		
+		int expected = 10;
+		assertEquals (expected,actual);
+	}
+	
+	
+	public void testDivide() {
+		int a = 10;
+		int b = 2;
+		
+		Calculator cal = new Calculator();
+		int actual = cal.divide(a, b);
+		
+		int expected = 5;
+		assertEquals (expected,actual);
+	}
 }
